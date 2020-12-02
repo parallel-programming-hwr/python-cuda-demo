ea3b090f6dbf3506e943dc76e7f73e08bea87603
Max Ehrlicher-Schmidt <maxes2000@t-online.de>
2020-12-02 14:44:38 +0100

    Add logging

--- primesGPU.py
+++ primesGPU.py
@@ -1,3 +1,4 @@
+import math
 from time import time
 
 import pycuda.autoinit
@@ -5,6 +6,8 @@ import pycuda.driver as drv
 import numpy as np
 from pycuda import gpuarray
 from pycuda.compiler import SourceModule
+
+from optparse import OptionParser
 
 ker = SourceModule("""
 __global__ void
@@ -62,28 +65,66 @@ __global__ void check_prime2(const unsigned long *IN, bool *OUT) {
 }
 """)
 
-block_size = 1024
-grid_size = 50000
 
-check_prime = ker2.get_function("check_prime2")
+def calc_primes(start: int = 1, grid_size: int = 1000, block_size: int = 1024):
+    check_prime = ker2.get_function("check_prime2")
 
-testvec = np.arange(1, block_size * grid_size * 2, step=2).astype(np.uint)
+    primes = []
+    if start < 2:
+        primes = [2]
+        start = 3
+    if start % 2 == 0:
+        start = start + 1
 
-testvec_gpu = gpuarray.to_gpu(testvec)
-outvec_gpu = gpuarray.to_gpu(np.full(block_size * grid_size, False, dtype=bool))
-t1 = time()
-check_prime(testvec_gpu, outvec_gpu, block=(block_size, 1, 1), grid=(grid_size, 1, 1))
-result = outvec_gpu.get()
-t2 = time()
+    testvec = np.arange(start, block_size * grid_size * 2 + start, step=2).astype(np.uint)
 
-primes = []
+    testvec_gpu = gpuarray.to_gpu(testvec)
+    outvec_gpu = gpuarray.to_gpu(np.full(block_size * grid_size, False, dtype=bool))
+    t1 = time()
+    check_prime(testvec_gpu, outvec_gpu, block=(block_size, 1, 1), grid=(grid_size, 1, 1))
+    result = outvec_gpu.get()
+    t2 = time()
 
-for idx, val in enumerate(result):
-    if val:
-        primes.append(idx)
+    for idx, val in enumerate(result):
+        if val:
+            primes.append(testvec[idx])
 
-#print(primes)
-print(len(primes))
-print('checked the first ' + str(block_size * grid_size) + ' numbers')
-print('last prime: ' + str(primes[-1]))
-print('The GPU needed ' + str(t2 - t1) + ' seconds')
+    print(len(primes))
+    print('checked ' + str(block_size * grid_size) + ' numbers')
+    print('last prime: ' + str(primes[-1]))
+    print('The GPU needed ' + str(t2 - t1) + ' seconds')
+
+    with open(options.timings_output, 'a') as file:
+        file.write(str(start) + "," + str((t2 - t1) * 1000) + "\n")
+
+    return primes
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-e", "--end", dest="end",
+                      help="numbers to check without even numbers", default="1000000000", type="int")
+    parser.add_option("--numbers-per-step", dest="numbers_per_step",
+                      help="amount of uneven numbers checked in each step (even number are skipped)", default="8388608",
+                      type="int")
+    parser.add_option("--output", dest="output",
+                      help="name of the file, where the primes should be stored", default="primes.txt", type="string")
+    parser.add_option("--timings-output", dest="timings_output",
+                      help="name of the csv file, where the timing is logged as csv", default="timings.csv",
+                      type="string")
+    (options, args) = parser.parse_args()
+
+    block_size = 1024
+    start = 1
+    grid_size = int(math.ceil(options.numbers_per_step / block_size))  # half of numbers per
+    resulting_numbers_per_step = block_size * grid_size
+    last_number_checked = start - 1
+
+    with open(options.timings_output, 'w') as file:
+        file.write("offset, duration\n")
+
+    while last_number_checked < options.end:
+        calculated_primes = calc_primes(last_number_checked + 1, grid_size, block_size)
+        with open(options.output, 'w') as file:
+            file.write("\n".join([str(p) for p in calculated_primes]))
+        last_number_checked = last_number_checked + resulting_numbers_per_step * 2
